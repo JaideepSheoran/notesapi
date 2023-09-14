c6c5e3bc5ab9ca23e7205fadca7da1ade240bbc3
JaideepSheoran <jaideepsinghsheoran@gmail.com>
2023-09-14 21:30:42 +0530

    night

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const allowedOrigins = ['https://inotes-react.web.app'];
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
